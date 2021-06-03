0e49f86a964f924d6d93e98aef25301cad0645fc
Michael Clemen <michael.clemen@gmail.com>
2021-06-03 15:57:02 +0800

    Update index.jsx
    update index.jsx

--- src/app/pages/News/index.jsx
+++ src/app/pages/News/index.jsx
@@ -1,10 +1,18 @@
 import markdown from "./news.md";
 import { htmlToVdom } from "hyperstatic/src/htmlToVdom";
 import { Link } from "hyperstatic";
+import News from "../News";
 
-export default () => (
-  <div class="news_section">
-    {htmlToVdom(markdown)}
-    <p></p>
-  </div>
-);
+export default ({ fields }) => {
+  const { body, title, subtitle, featuredImage, address, phone, email } =
+    fields;
+  return (
+    <div className="Contact">
+      <PageHeader
+        title={title}
+        subtitle={subtitle}
+        backgroundImage={featuredImage}
+      />
+    </div>
+  );
+};
