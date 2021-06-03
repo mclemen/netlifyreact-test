91ccd58ad59aac034050c3c36e5ea30d8aaf44f5
Michael Clemen <michael.clemen@gmail.com>
2021-06-03 15:58:49 +0800

    Update index.jsx
    index

--- src/app/pages/News/index.jsx
+++ src/app/pages/News/index.jsx
@@ -6,13 +6,9 @@ import News from "../News";
 export default ({ fields }) => {
   const { body, title, subtitle, featuredImage, address, phone, email } =
     fields;
-  return (
-    <div className="Contact">
-      <PageHeader
-        title={title}
-        subtitle={subtitle}
-        backgroundImage={featuredImage}
-      />
-    </div>
-  );
+  <div>
+    title={title}
+    subtitle={subtitle}
+    backgroundImage={featuredImage}
+  </div>;
 };
